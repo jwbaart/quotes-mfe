1442a123876fb03ec4f549eeea358ae9c00528e9
Jan-Willem Baart <jwbaart@gmail.com>
2022-01-17 13:12:52 +0100

    feat: quotes mfe app

---
+++ apps/quotes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quotes-mfe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'quotes';
+}
